65efcf03db23cf39a20015c08cd52cbd8fef5682
Ben Pedigo <benjamindpedigo@gmail.com>
2023-12-19 11:22:23 -0800

    chore: remove some old specs

--- networkframe/__init__.py
+++ networkframe/__init__.py
@@ -1,9 +1,5 @@
 """Top-level package for networkframe."""
 
-__author__ = """Ben Pedigo"""
-__email__ = "ben.pedigo@alleninstitute.org"
-__version__ = "0.3.0"
-
 from .networkframe import NetworkFrame
 
-__all__ = ["NetworkFrame", "test"]
+__all__ = ["NetworkFrame"]
--- networkframe/test/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Init file for test package."""
-
-from .test import my_func
-
-__all__ = ["my_func"]
