1e7e44b24ec02fdbf167a9868799c4297899bc4b
Ben Pedigo <benjamindpedigo@gmail.com>
2024-01-19 16:02:10 -0800

    add a version

--- networkframe/__init__.py
+++ networkframe/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for networkframe."""
 
+import pkg_resources
+
 from .networkframe import LocIndexer, NetworkFrame, NodeGroupBy
 
 __all__ = ["NetworkFrame", "NodeGroupBy", "LocIndexer"]
+
+__version__ = pkg_resources.get_distribution("networkframe").version
